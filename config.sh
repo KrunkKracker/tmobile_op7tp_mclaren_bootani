06cf4f9438fd1332560a57b0f7676d31840e5597
KrunkKracker <btfncn2q4m@pomail.net>
2020-03-10 12:27:06 -0400

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,25 +1,4 @@
 ##########################################################################################
-#
-# Magisk
-# by topjohnwu
-# 
-# This is a template zip for developers
-#
-##########################################################################################
-##########################################################################################
-# 
-# Instructions:
-# 
-# 1. Place your files into system folder (delete the placeholder file)
-# 2. Fill in your module's info into module.prop
-# 3. Configure the settings in this file (common/config.sh)
-# 4. For advanced features, add shell commands into the script files under common:
-#    post-fs-data.sh, service.sh
-# 5. For changing props, add your additional/modified props into common/system.prop
-# 
-##########################################################################################
-
-##########################################################################################
 # Defines
 ##########################################################################################
 
@@ -90,17 +69,4 @@ REPLACE="
 set_permissions() {
   # Default permissions, don't remove them
   set_perm_recursive  $MODPATH  0  0  0755  0644
-
-  # Only some special files require specific permissions
-  # The default permissions should be good enough for most cases
-
-  # Some templates if you have no idea what to do:
-
-  # set_perm_recursive  <dirname>                <owner> <group> <dirpermission> <filepermission> <contexts> (default: u:object_r:system_file:s0)
-  # set_perm_recursive  $MODPATH/system/lib       0       0       0755            0644
-
-  # set_perm  <filename>                         <owner> <group> <permission> <contexts> (default: u:object_r:system_file:s0)
-  # set_perm  $MODPATH/system/bin/app_process32   0       2000    0755         u:object_r:zygote_exec:s0
-  # set_perm  $MODPATH/system/bin/dex2oat         0       2000    0755         u:object_r:dex2oat_exec:s0
-  # set_perm  $MODPATH/system/lib/libart.so       0       0       0644
 }
